d920f062eea5010b329328a45cf82449394d7143
seoyun-tech <cielle.sora@google.com>
2026-04-03 13:32:18 +0900

    feat: EyeGuard 기능 추가

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,24 @@
-import { StrictMode } from "react";
+import { StrictMode, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Navigate } from "react-router";
 import App from "./App.jsx";
-import MainPage from "./pages/MainPage";
-import MainPageJunior from "./pages/MainPageJunior";
-import SearchPage from "./pages/SearchPage";
-import MyPage from "./pages/MyPage";
-import DetailPage from "./pages/DetailPage";
 import "./styles/index.css";
+
+const MainPage = lazy(() => import("./pages/MainPage"));
+const MainPageJunior = lazy(() => import("./pages/MainPageJunior"));
+const SearchPage = lazy(() => import("./pages/SearchPage"));
+const MyPage = lazy(() => import("./pages/MyPage"));
+const DetailPage = lazy(() => import("./pages/DetailPage"));
 
 const router = createBrowserRouter([
   {
     element: <App />,
     children: [
-      { path: "/",              element: <MainPage /> },
-      { path: "/junior",        element: <MainPageJunior /> },
-      { path: "/search",        element: <SearchPage /> },
-      { path: "/mypage",        element: <MyPage /> },
-      { path: "/movie/:movieId",element: <DetailPage /> },
+      { path: "/",              element: <Suspense><MainPage /></Suspense> },
+      { path: "/junior",        element: <Suspense><MainPageJunior /></Suspense> },
+      { path: "/search",        element: <Suspense><SearchPage /></Suspense> },
+      { path: "/mypage",        element: <Suspense><MyPage /></Suspense> },
+      { path: "/movie/:movieId",element: <Suspense><DetailPage /></Suspense> },
       { path: "*",              element: <Navigate to="/" replace /> },
     ],
   },
